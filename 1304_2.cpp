7b7f75fcc7a7b13dd3331fbcbb8f157e17a75476
raphael <37984884+rafaax@users.noreply.github.com>
2022-04-15 12:33:52 -0300

    Add files via upload

---
+++ 1304_2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdio.h>
+main()
+{
+
+//==================//*
+//GUILHERME HENRIQUE MACHADO RIBEIRO //
+// RAPHAEL GUSTAVO MEIRELES//
+//==================//*
+	
+//==================//*
+//DECLARAÇÃO DE VARIAVEIS//
+//==================//*
+	
+float gasolina = 7.04;
+float alcool = 5.09;	
+int km = 0;
+float milhas = 0;
+float metros = 0;
+float centimetros = 0;
+float consumo = 0;
+float consumo2 = 0;
+printf("Quantos km voce rodou com seu carro? = \n");
+	scanf("%d", &km);
+consumo = (km/10)*gasolina;
+consumo2 = (km/8)*alcool;		
+printf("Voce gastou %f reais de gasolina ou %f de alcool tendo em vista que a gasolina custa %f e o alcool %f \n", consumo, consumo2, gasolina, alcool);
+printf("\n");
+
+milhas = km * 0.62137;
+metros = km / 0.001;
+centimetros = km / 0.00001;
+printf("%d km e igual a %f milhas %f metros e %f centimentros", km, milhas, metros, centimetros);
+
+	
+}
